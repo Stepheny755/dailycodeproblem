324806a37f59085b06d0aface304849c4333e815
Stephen Yang <stepheny755@gmail.com>
2019-04-17 14:13:10 -0700

    renamed to d1.py

--- d1.py
+++ d1.py
@@ -1,33 +1,14 @@
 '''
-Given an array of integers, return a new array such that each element at index i of the new array
-is the product of all the numbers in the original array except the one at i.
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 
-For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24].
-If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-
-Follow-up: what if you can't use division?
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 '''
-import numpy as np
 
-def d1(inp):
-    p = 1
-    a = [0]*(len(inp))
+def d1(inp,k):
     for i in range(len(inp)):
-        a[i] = p
-        p = p * inp[i]
-    p = 1
-    b = [0]*(len(inp))
-    print(b)
-    for y in range(len(inp)):
-        i = len(inp)-y-1
-        print(i)
-        b[i] = p
-        p = p * inp[i]
-    out = [0]*(len(inp))
-    for i in range(len(inp)):
-        out[i] = a[i]*b[i]
-    return out
+        t1 = k-inp[i]
+        if t1 in inp:
+            return True
+    return False
 
-inp = [3,2,1]
-print(len(inp))
-print(d1(inp))
+print(d1([10,15,3,7],17))
